ba6921e8db6ae53977993db323657685e63d06b4
AlbaGuzman25 <albasofiaguzmanarias@gmail.com>
2024-09-09 15:19:40 -0500

    controladores sofia

---
+++ Backend/API PESO_PIG/Controllers/Corral.Controller.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PESOPIG.Models;
+
+namespace PESOPIG.Controllers
+{
+    [ApiController]
+    [Route("Api/controller")]
+    public class CorralController : Controller
+    {
+        [HttpPost("CreateCorral")]
+        public IActionResult CreateCorral
+            (CorralModel etapa)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }        
+        }
+        [HttpGet("GetCorrales")]
+        public IActionResult GetCorrales()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+           
+        }
+        [HttpGet("GetCorral")]
+        public IActionResult GetCorral()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            
+        }
+        [HttpPut("UpdateCorral")]
+        public IActionResult UpdateEtapa(CorralModel Corral)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+            
+        }
+        [HttpDelete("DeleteCorral")]
+        public IActionResult DeleteCorral()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+
+            }
+            
+        }
+    }
+}
---
+++ Backend/API PESO_PIG/Controllers/Stage.Controller.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PESOPIG.Models;
+
+namespace PESOPIG.Controllers
+{
+    [ApiController]
+    [Route("Api/controller")]
+    public class StageController : Controller
+    {
+        [HttpPost("CreateStage")]
+        public IActionResult CreateStage
+            (StageModel stage)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+        }
+        [HttpGet("GetStages")]
+        public IActionResult GetStages()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+        }
+        [HttpGet("GetEtapa")]
+        public IActionResult GetStage()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }            
+        }
+        [HttpPost("UpdateEtapa")]
+        public IActionResult UpdateStage(StageModel Stage)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+            }
+        }
+        [HttpDelete("DeleteStage")]
+        public IActionResult DeleteStage()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message.ToString());
+
+            } 
+        }
+    }
+
+}
