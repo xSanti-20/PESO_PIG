a183ae0fb7b7dc48925ae62d72c9db1e35be4272
unknown <SENA@ESPAPRCAJFSP052.FormacionSena.RED>
2024-09-09 14:58:28 -0500

    modelos danna

---
+++ Backend/API PESO_PIG/Models/InCharge.Models.cs
@@ -0,0 +1,10 @@
+﻿namespace PESO_PIG.Models
+{
+    public class InChargeModels
+    {
+         public string id_InCharge {  get; set; }
+        public string nam_InCharge { get; set; }
+        public string  rol_InCharge { get; set; }
+        public string  guy_InCharge { get; set; }
+    }
+}
