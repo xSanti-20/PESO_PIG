fcfb0cedaceb74eaad93ecadc77a053ca1e312a4
xSanti-20 <puentessantiago2003@gmail.com>
2025-03-27 11:01:24 -0500

    bd nueva

--- Database/NEWBDPESOPIG.sql
+++ Database/NEWBDPESOPIG.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
 -- Host: localhost    Database: peso_pig
 -- ------------------------------------------------------
--- Server version	8.0.39
+-- Server version	8.0.41
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,11 +23,11 @@ DROP TABLE IF EXISTS `corrals`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `corrals` (
-  `id_Corral` int NOT NULL,
+  `id_Corral` int NOT NULL AUTO_INCREMENT,
   `Des_Corral` varchar(200) NOT NULL,
   `Tot_Animal` int NOT NULL,
   PRIMARY KEY (`id_Corral`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,37 @@ CREATE TABLE `corrals` (
 
 LOCK TABLES `corrals` WRITE;
 /*!40000 ALTER TABLE `corrals` DISABLE KEYS */;
+INSERT INTO `corrals` VALUES (1,'corral 2',7);
 /*!40000 ALTER TABLE `corrals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entries`
+--
+
+DROP TABLE IF EXISTS `entries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entries` (
+  `id_Entries` int NOT NULL AUTO_INCREMENT,
+  `vlr_Entries` int NOT NULL,
+  `Fec_Entries` datetime NOT NULL,
+  `Fec_Expiration` datetime NOT NULL,
+  `Can_Food` int NOT NULL,
+  `id_Food` int NOT NULL,
+  PRIMARY KEY (`id_Entries`),
+  KEY `fk_entries_foods_idx` (`id_Food`),
+  CONSTRAINT `fk_entries_foods` FOREIGN KEY (`id_Food`) REFERENCES `foods` (`id_Food`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entries`
+--
+
+LOCK TABLES `entries` WRITE;
+/*!40000 ALTER TABLE `entries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entries` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -51,7 +81,16 @@ CREATE TABLE `feedings` (
   `Des_Feeding` varchar(200) NOT NULL,
   `Can_Feeding` int NOT NULL,
   `Con_Average` int NOT NULL,
-  PRIMARY KEY (`id_Feeding`)
+  `id_Users` int NOT NULL,
+  `id_Piglet` int NOT NULL,
+  `id_Food` int NOT NULL,
+  PRIMARY KEY (`id_Feeding`),
+  KEY `fk_feedingd_users_idx` (`id_Users`),
+  KEY `fk_feedings_piglet_idx` (`id_Piglet`),
+  KEY `fk_feedings_foods_idx` (`id_Food`),
+  CONSTRAINT `fk_feedings_foods` FOREIGN KEY (`id_Food`) REFERENCES `foods` (`id_Food`),
+  CONSTRAINT `fk_feedings_piglet` FOREIGN KEY (`id_Piglet`) REFERENCES `piglets` (`id_Piglet`),
+  CONSTRAINT `fk_feedings_users` FOREIGN KEY (`id_Users`) REFERENCES `users` (`id_Users`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -79,8 +118,11 @@ CREATE TABLE `foods` (
   `Vlr_Unit` int NOT NULL,
   `Fec_Expiration` date DEFAULT NULL,
   `Und_Extent` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_Food`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_Stage` int NOT NULL,
+  PRIMARY KEY (`id_Food`),
+  KEY `fk_food_stages_idx` (`id_Stage`),
+  CONSTRAINT `fk_foods_stages` FOREIGN KEY (`id_Stage`) REFERENCES `stages` (`id_Stage`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,7 +131,7 @@ CREATE TABLE `foods` (
 
 LOCK TABLES `foods` WRITE;
 /*!40000 ALTER TABLE `foods` DISABLE KEYS */;
-INSERT INTO `foods` VALUES (1,'precebo','',0,0,NULL,''),(2,'levante','',0,0,NULL,'');
+INSERT INTO `foods` VALUES (1,'concentrado','nose',10,15000,'2026-04-20','10',1);
 /*!40000 ALTER TABLE `foods` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,8 +149,17 @@ CREATE TABLE `piglets` (
   `Weight_Initial` int NOT NULL,
   `Acum_Weight` int NOT NULL,
   `Sex_Piglet` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_Piglet`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_Stage` int NOT NULL,
+  `id_Race` int NOT NULL,
+  `id_Corral` int NOT NULL,
+  PRIMARY KEY (`id_Piglet`),
+  KEY `fk_piglets_stages_idx` (`id_Stage`),
+  KEY `fk_piglets_races_idx` (`id_Race`),
+  KEY `fk_piglets_corrals_idx` (`id_Corral`),
+  CONSTRAINT `fk_piglets_corrals` FOREIGN KEY (`id_Corral`) REFERENCES `corrals` (`id_Corral`),
+  CONSTRAINT `fk_piglets_races` FOREIGN KEY (`id_Race`) REFERENCES `races` (`id_Race`),
+  CONSTRAINT `fk_piglets_stages` FOREIGN KEY (`id_Stage`) REFERENCES `stages` (`id_Stage`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -117,7 +168,7 @@ CREATE TABLE `piglets` (
 
 LOCK TABLES `piglets` WRITE;
 /*!40000 ALTER TABLE `piglets` DISABLE KEYS */;
-INSERT INTO `piglets` VALUES (10,'pepe','2024-12-11',23,0,'Hembra'),(11,'nutri lechon','2024-12-11',120,0,'Macho'),(12,'maria','2024-12-10',12,0,'Hembra'),(13,'jose','2024-12-03',10,0,'Macho'),(14,'lili','2024-12-11',25,0,'Hembra');
+INSERT INTO `piglets` VALUES (15,'Paco','2025-03-16',15,15,'Macho',1,1,1),(16,'lula','2025-03-19',10,10,'Hembra',1,1,1);
 /*!40000 ALTER TABLE `piglets` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,6 +192,7 @@ CREATE TABLE `races` (
 
 LOCK TABLES `races` WRITE;
 /*!40000 ALTER TABLE `races` DISABLE KEYS */;
+INSERT INTO `races` VALUES (1,'triple jamon');
 /*!40000 ALTER TABLE `races` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,6 +219,7 @@ CREATE TABLE `stages` (
 
 LOCK TABLES `stages` WRITE;
 /*!40000 ALTER TABLE `stages` DISABLE KEYS */;
+INSERT INTO `stages` VALUES (1,'Precebo',5,10,20);
 /*!40000 ALTER TABLE `stages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,8 +240,10 @@ CREATE TABLE `users` (
   `Token` varchar(255) DEFAULT NULL,
   `Hashed_Password` varchar(60) DEFAULT NULL,
   `Salt` varchar(250) DEFAULT NULL,
+  `ResetToken` varchar(250) DEFAULT NULL,
+  `ResetTokenExpiration` datetime DEFAULT NULL,
   PRIMARY KEY (`id_Users`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -197,7 +252,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (10,'Santiago','Aprendiz','puentessantiago2003@gmail.com',0,0,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InB1ZW50ZXNzYW50aWFnbzIwMDNAZ21haWwuY29tIiwiVXNlcklkIjoiMTAiLCJuYmYiOjE3MzM4NzEwNDcsImV4cCI6MTczMzg3NDY0NywiaWF0IjoxNzMzODcxMDQ3fQ.v3fSeqW3Rm_a4JTVvWI-75pitMLC3QaOolp2Lfa9aSI','$2a$11$5ldVGvJL36BqzsyfeZBCPu1gcfhTB1TidPCl/iyvyMtJ5qUxl85LW','$2a$11$6oRlNzMjqSTP8OyJuVkXse'),(11,'danna','Aprendiz','danna30@gmail.com',0,0,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhbm5hMzBAZ21haWwuY29tIiwiVXNlcklkIjoiMTEiLCJuYmYiOjE3MzIxMzE2MjAsImV4cCI6MTczMjEzNTIyMCwiaWF0IjoxNzMyMTMxNjIwfQ.UDbvZDWCVomnRRAs06rneKsW6-rneqWuv5We0QRduUg','$2a$11$A3vELlskN7121ZNA8mDUPeDGYz00MsGrALCtKSlSEacJ8m41FJfbi','$2a$11$VrFX2X4MB1v7MO6.SMR7be'),(12,'Pepe','Aprendiz','pepe@gmail.com',0,0,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBlcGVAZ21haWwuY29tIiwiVXNlcklkIjoiMTIiLCJuYmYiOjE3MzM5MjM4MDYsImV4cCI6MTczMzkyNzQwNiwiaWF0IjoxNzMzOTIzODA2fQ.nX1Hm703hncfn0EdEv2VrCZX2gvDnrkBg4-K5rYtklk','$2a$11$q6vhHl8Kyrgh5.82eC0A4.XWOA4AOWYrEQViRD2ws69Yt.x1eaSbe','$2a$11$mOkl9Igycc1f7g0sOwvR0u'),(13,'asdf','Aprendiz','sofi@gmail.com',0,0,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNvZmlAZ21haWwuY29tIiwiVXNlcklkIjoiMTMiLCJuYmYiOjE3MzM5MjUyMzQsImV4cCI6MTczMzkyODgzNCwiaWF0IjoxNzMzOTI1MjM0fQ.6HjWYJrxPDFrVwdsGTrSzwxNrM-g3ID0n7sKidlj67M','$2a$11$B0dXhdvzN.tsr9iLMHzqM.2w6jxhqhXUhIdilvDcyDFyYFFRgg1bi','$2a$11$zVmqfBCWGeN4hwAHqIcA6e');
+INSERT INTO `users` VALUES (14,'santiago','Aprendiz','santiago@gmail.com',0,0,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbnRpYWdvQGdtYWlsLmNvbSIsIlVzZXJJZCI6IjE0IiwibmJmIjoxNzQyMDU0NjQ0LCJleHAiOjE3NDIwNTgyNDQsImlhdCI6MTc0MjA1NDY0NH0.avKWslMVhYbS860EM5CGjQcAXF8Yo9G9ZpYI0uoTnRA','$2a$11$50CKm5WfkqfsYkf7sU89We3qtmBQTvWf4.CjizP.lvm3996XpKhoK','$2a$11$YPZsrVEbnFUs0/ZR7zeeG.',NULL,NULL),(15,'santiago','Aprendiz','puentessantiago2003@gmail.com',0,0,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InB1ZW50ZXNzYW50aWFnbzIwMDNAZ21haWwuY29tIiwiVXNlcklkIjoiMTUiLCJuYmYiOjE3NDMwMjY3MDYsImV4cCI6MTc0MzAzMDMwNiwiaWF0IjoxNzQzMDI2NzA2fQ.P5NBML0bfIh18KtiZWKCiaPQjJCc__n3t-S3DPa-S9k','$2a$11$CwHsCYOTQlsPDihAbEhUK.2xVf6yQwqlqnUA4Yew4rRztFMVdlYOG','$2a$11$CBOhaeKD/deq5pRWAX9qmO','ZmQ2OGY2MWQtMzRkZi00ZWQ2LWE1NzctMTU0MDBmYWM2YjAw','2025-03-26 22:31:06');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -213,7 +268,13 @@ CREATE TABLE `weighings` (
   `Weight_Current` int NOT NULL,
   `Weight_Gain` int NOT NULL,
   `Fec_Weight` date DEFAULT NULL,
-  PRIMARY KEY (`id_Weighings`)
+  `id_Users` int NOT NULL,
+  `id_Piglet` int NOT NULL,
+  PRIMARY KEY (`id_Weighings`),
+  KEY `fk_weighings_users_idx` (`id_Users`),
+  KEY `fk_weighings_piglet_idx` (`id_Piglet`),
+  CONSTRAINT `fk_weighings_piglet` FOREIGN KEY (`id_Piglet`) REFERENCES `piglets` (`id_Piglet`),
+  CONSTRAINT `fk_weighings_users` FOREIGN KEY (`id_Users`) REFERENCES `users` (`id_Users`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -223,7 +284,6 @@ CREATE TABLE `weighings` (
 
 LOCK TABLES `weighings` WRITE;
 /*!40000 ALTER TABLE `weighings` DISABLE KEYS */;
-INSERT INTO `weighings` VALUES (1,0,15,NULL),(2,0,4,NULL),(3,0,4,NULL);
 /*!40000 ALTER TABLE `weighings` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -236,4 +296,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-10  9:31:04
+-- Dump completed on 2025-03-27 11:00:16
